6d0250e1f6b13db508acb88e571134a124814808
Clueroi <eriicsouzaromeroo@gmail.com>
2026-07-13 23:22:33 -0300

    Docs: Changed Signup Folder

--- src/app/signup/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignupForm } from "@/src/components/auth/signup-form";
-
-export default function SignupPage() {
-  return (
-    <main className="flex items-center justify-center min-h-screen">
-      <SignupForm />
-    </main>
-  );
-}
